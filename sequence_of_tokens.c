0015e1f809eccefcf42560781e2ca652b9396885
Arijit Ghosh <91056395+Ari-Ghosh@users.noreply.github.com>
2024-04-07 21:04:09 +0530

    Create sequence_of_tokens.c

---
+++ sequence_of_tokens.c
@@ -0,0 +1,218 @@
+// 1) Write a program in C to convert a declaration statement into a sequence of tokens
+// 2) Write a program in C to convert a control flow statements (conditions and loops into a sequence of tokens.
+// 3) Write a program in C to convert a 1/0 statements (printf, scanf) into a sequence of tokens.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAX_TOKENS 100
+#define MAX_TOKEN_LENGTH 100
+
+typedef enum {
+    KEYWORD,
+    IDENTIFIER,
+    CONSTANT,
+    STRING_LITERAL,
+    PUNCTUATOR,
+    COMMENT,
+    PREPROCESSOR,
+    OTHER
+} TokenType;
+
+typedef struct {
+    TokenType type;
+    char value[MAX_TOKEN_LENGTH];
+} Token;
+
+int isIdentifier(char *token);
+int isConstant(char *token);
+int isStringLiteral(char *token);
+int isComment(char *token);
+
+int isKeyword(char *token) {
+    char *keywords[] = {
+        "auto", "break", "case", "char", "const", "continue", "default", "do",
+        "double", "else", "enum", "extern", "float", "for", "goto", "if",
+        "int", "long", "register", "return", "short", "signed", "sizeof",
+        "static", "struct", "switch", "typedef", "union", "unsigned", "void",
+        "volatile", "while"
+    };
+    int numKeywords = sizeof(keywords) / sizeof(keywords[0]);
+
+    for (int i = 0; i < numKeywords; i++) {
+        if (strcmp(token, keywords[i]) == 0)
+            return 1;
+    }
+
+    return 0;
+}
+
+int isPunctuator(char *token) {
+    char *punctuators[] = {
+        "[", "]", "(", ")", "{", "}", ".", "->", "++", "--", "&", "*", "+", "-",
+        "~", "!", "/", "%", "<<", ">>", "<", ">", "<=", ">=", "==", "!=", "^",
+        "|", "&&", "||", "?", ":", ";", "...", "=", "*=", "/=", "%=", "+=",
+        "-=", "<<=", ">>=", "&=", "^=", "|=", ",", "#", "##", "<:", ":>", "<%",
+        "%>", "%:", "%:%:"
+    };
+    int numPunctuators = sizeof(punctuators) / sizeof(punctuators[0]);
+
+    for (int i = 0; i < numPunctuators; i++) {
+        if (strcmp(token, punctuators[i]) == 0)
+            return 1;
+    }
+
+    return 0;
+}
+
+int isPreprocessor(char *token) {
+    char *preprocessors[] = {
+        "include", "define", "undef", "line", "error", "pragma"
+    };
+    int numPreprocessors = sizeof(preprocessors) / sizeof(preprocessors[0]);
+
+    for (int i = 0; i < numPreprocessors; i++) {
+        if (strcmp(token, preprocessors[i]) == 0)
+            return 1;
+    }
+
+    return 0;
+}
+
+int isOther(char *token) {
+    if (!isKeyword(token) && !isPunctuator(token) && !isPreprocessor(token)
+        && !isIdentifier(token) && !isConstant(token) && !isStringLiteral(token)
+        && !isComment(token))
+        return 1;
+
+    return 0;
+}
+
+int isIdentifier(char *token) {
+    if (isalpha(token[0]) || token[0] == '_')
+        return 1;
+
+    return 0;
+}
+
+int isConstant(char *token) {
+    if (isdigit(token[0]))
+        return 1;
+
+    return 0;
+}
+
+int isStringLiteral(char *token) {
+    if (token[0] == '"')
+        return 1;
+
+    return 0;
+}
+
+int isComment(char *token) {
+    if (token[0] == '/' && token[1] == '/')
+        return 1;
+
+    return 0;
+}
+
+void printToken(Token token) {
+    switch (token.type) {
+        case KEYWORD:
+            printf("Keyword: %s\n", token.value);
+            break;
+        case IDENTIFIER:
+            printf("Identifier: %s\n", token.value);
+            break;
+        case CONSTANT:
+            printf("Constant: %s\n", token.value);
+            break;
+        case STRING_LITERAL:
+            printf("String literal: %s\n", token.value);
+            break;
+        case PUNCTUATOR:
+            printf("Punctuator: %s\n", token.value);
+            break;
+        case COMMENT:
+            printf("Comment: %s\n", token.value);
+            break;
+        case PREPROCESSOR:
+            printf("Preprocessor: %s\n", token.value);
+            break;
+        case OTHER:
+            printf("Other: %s\n", token.value);
+            break;
+    }
+}
+
+int main() {
+    char input[1000];
+    char *token;
+    Token tokens[MAX_TOKENS];
+    int numTokens = 0;
+
+    printf("Enter a statement: ");
+    fgets(input, sizeof(input), stdin);
+
+    token = strtok(input, " \n\t");
+    while (token != NULL) {
+        if (isKeyword(token)) {
+            tokens[numTokens].type = KEYWORD;
+            strcpy(tokens[numTokens].value, token);
+        } else if (isPunctuator(token)) {
+            tokens[numTokens].type = PUNCTUATOR;
+            strcpy(tokens[numTokens].value, token);
+        } else if (isPreprocessor(token)) {
+            tokens[numTokens].type = PREPROCESSOR;
+            strcpy(tokens[numTokens].value, token);
+        } else if (isOther(token)) {
+            tokens[numTokens].type = OTHER;
+            strcpy(tokens[numTokens].value, token);
+        } else if (isIdentifier(token)) {
+            tokens[numTokens].type = IDENTIFIER;
+            strcpy(tokens[numTokens].value, token);
+        } else if (isConstant(token)) {
+            tokens[numTokens].type = CONSTANT;
+            strcpy(tokens[numTokens].value, token);
+        } else if (isStringLiteral(token)) {
+            tokens[numTokens].type = STRING_LITERAL;
+            strcpy(tokens[numTokens].value, token);
+        } else if (isComment(token)) {
+            tokens[numTokens].type = COMMENT;
+            strcpy(tokens[numTokens].value, token);
+        } else {
+            printf("Error: Invalid token\n");
+            return 1;
+        }
+
+        numTokens++;
+        token = strtok(NULL, " \n\t");
+    }
+
+    printf("\nTokens:\n");
+    for (int i = 0; i < numTokens; i++)
+        printToken(tokens[i]);
+
+    return 0;
+}
+
+// Output:
+// Enter a statement: int main () { printf ( " Hello World!\n " ) ; return 0 ; }
+//
+// Tokens:
+// Keyword: int
+// Identifier: main
+// Punctuator: (
+// Punctuator: )
+// Punctuator: {
+// Identifier: printf
+// Punctuator: (
+// String literal: "Hello, world!\n"
+// Punctuator: )
+// Punctuator: ;
+// Keyword: return
+// Constant: 0
+// Punctuator: ;
+// Punctuator: }
